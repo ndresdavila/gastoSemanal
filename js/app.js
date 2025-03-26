153a57c41aa2de3ca6b24d9429cae939ecde7876
Andres Davila <andresdavilasanchez@gmail.com>
2025-03-26 16:19:53 -0500

    Finished project: expenses can be added and removed and ui reacts accordingly

--- js/app.js
+++ js/app.js
@@ -19,8 +19,18 @@ class Presupuesto {
     }
 
     nuevoGasto(gasto) {
-        this.gastos = [...this.gastos, gasto]
-        console.log(this.gastos)
+        this.gastos = [...this.gastos, gasto];
+        this.calcularRestante();
+    }
+
+    calcularRestante() {
+        const gastado = this.gastos.reduce( (total, gasto) => total + gasto.cantidad, 0 );
+        this.restante = this.presupuesto - gastado;
+    }
+
+    eliminarGasto(id) {
+        this.gastos = this.gastos.filter( gasto => gasto.id !== id);
+        this.calcularRestante();
     }
 }
 
@@ -54,6 +64,73 @@ class UI {
         setTimeout(() => {
             divMensaje.remove();
         }, 3000);
+    }
+
+    mostrarGastos(gastos) {
+        // elimina html previo
+        this.limpiarHTML();
+
+        // iterar sobre los gastos
+        gastos.forEach( gasto => {
+            const {cantidad, nombre, id} = gasto;
+
+            // crear li
+            const nuevoGasto = document.createElement('li');
+            nuevoGasto.className = 'list-group-item d-flex justify-content-between align-items-center';
+            
+            //nuevoGasto.setAttribute('data-id', id); // deprecated
+            nuevoGasto.dataset.id = id// crea atributo data-id
+
+            console.log(nuevoGasto)
+
+            // agregar html del gasto
+            nuevoGasto.innerHTML = `${nombre} <span class="badge badge-primary badge-pill"> $${cantidad}</span>`
+
+            // botón para borrar gasto
+            const btnBorrar = document.createElement('button');
+            btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
+            btnBorrar.innerHTML = 'Borrar &times';
+            btnBorrar.onclick = () => { eliminarGasto(id)};
+            nuevoGasto.appendChild(btnBorrar);
+
+            // agregar al html
+            gastoListado.appendChild(nuevoGasto);
+
+        })
+    }
+
+    limpiarHTML() {
+        while(gastoListado.firstChild) {
+            gastoListado.removeChild(gastoListado.firstChild);
+        }
+    }
+
+    actualizarRestante(restante) {
+        document.querySelector('#restante').textContent = restante;
+    }
+
+    comprobarPresupuesto(presupuestoObj) {
+        const {presupuesto, restante} = presupuestoObj;
+        const restanteDiv = document.querySelector('.restante');
+
+        // comprobar 25%: pinta el mensaje de rojo
+        if((presupuesto / 4) > restante) {
+            restanteDiv.classList.remove('alert-success', 'alert-warning');
+            restanteDiv.classList.add('alert-danger');
+        // comprobar 50%
+        } else if ((presupuesto / 2) > restante){
+            restanteDiv.classList.remove('alert-success');
+            restanteDiv.classList.add('alert-warning');
+        } else {
+            restanteDiv.classList.remove('alert-danger', 'alert-warning');
+            restanteDiv.classList.add('alert-success');
+        }
+
+        // si el total es 0 o menor
+        if(restante <= 0) {
+            ui.imprimirAlerta('El presupuesto se ha agotado', 'error');
+            formulario.querySelector('button[type="submit"]').disabled = true;
+        }
     }
 }
 
@@ -97,6 +174,25 @@ function agregarGasto(e) {
     presupuesto.nuevoGasto(gasto);
     // muestra mensaje de éxito
     ui.imprimirAlerta('Gasto agregado correctamente');
+
+    // imprimir los gastos
+    const { gastos, restante } = presupuesto;
+    ui.mostrarGastos(gastos);
+
+    ui.actualizarRestante(restante);
+
+    ui.comprobarPresupuesto(presupuesto);
+
     // reinicia el formulario
     formulario.reset();
+}
+
+function eliminarGasto(id) {
+    // elimina gasto del objeto
+    presupuesto.eliminarGasto(id);
+    // elimina gasto del html
+    const {gastos, restante} = presupuesto;
+    ui.mostrarGastos(gastos);
+    ui.actualizarRestante(restante);
+    ui.comprobarPresupuesto(presupuesto);
 }
