d4ec3b3df44183302245acbc3f75b822b085745e
Andres Davila <andresdavilasanchez@gmail.com>
2025-03-26 10:35:15 -0500

    Implementation of classes, user interface and form validation

--- js/app.js
+++ js/app.js
@@ -6,18 +6,86 @@ const gastoListado = document.querySelector('#gastos ul')
 eventListeners()
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+
+    formulario.addEventListener('submit', agregarGasto);
 }
 
 // clases
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto); // al inicio el presupuesto y restante son iguales
+        this.gastos = [];
+    }
+}
 
+class UI {
+    insertarPresupuesto(cantidad) {
+        // extrae valores del parámetro (destructuring)
+        const {presupuesto, restante} =  cantidad
+        // insertar alores de presupuesto y resntate en el texto del div respectivo
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
+
+    imprimirAlerta(mensaje, tipo) {
+        // crear div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center','alert'); // clases de bootstrap
+
+        if(tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-success');
+        }
+
+        // mensaje de error
+        divMensaje.textContent = mensaje;
+
+        //insertar en html
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+        //quitarlo del html
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
+}
+
+const ui = new UI(); // instancia del user interface
+let presupuesto; // instancia no inicializada de presupuesto
 
 // funciones
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('Cual es tu presupuesto?')
-    console.log(Number(presupuestoUsuario)) // convierte a número con coma
 
     // Invalid input format handler
     if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         window.location.reload() // recarga la ventana actual
     }
+
+    // Presupuesto válido
+    presupuesto = new Presupuesto(presupuestoUsuario); // inicialización (luego de validar input) del presupuesto
+    ui.insertarPresupuesto(presupuesto);
+}
+
+// agrega gastos
+function agregarGasto(e) {
+    e.preventDefault();
+
+    // leer los datos del formulario
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = document.querySelector('#cantidad').value;
+
+    // validar
+    if(nombre === '' || cantidad === '') {
+        ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
+        return;
+    } else if (cantidad <= 0 || isNaN(cantidad)) {
+        ui.imprimirAlerta('Cantidad no válida', 'error');
+        return;
+    }
+
+    // agrega el gasto
+    console.log('Agregando gasto...');
 }
